cc49c53eb946bbefd4a2418ef353d59a9c793c83
Andi Weber <adtweber@gmail.com>
2024-05-11 12:06:36 -0400

    created vercel file

---
+++ models/books.js
@@ -0,0 +1,13 @@
+// const mongoose = require('mongoose');
+// const { Schema } = mongoose
+
+// // SCHEMA
+// const bookSchema = new mongoose.Schema({
+//     title: { type: String, required: true },
+//     description: { type: String },
+//     year: { type: Number },
+//     quantity: { type: Number },
+//     imageUrl: { type: String }
+// })
+
+// module.exports = mongoose.model('Book', bookSchema);
---
+++ server.js
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+const express = require('express')
+// const mongoose = require('mongoose')
+// const methodOverride = require('method-override')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT
+const app = express()
+
+// MIDDLEWARE
+// app.set('views', __dirname + '/views')
+// app.set('view-engine', 'jsx')
+// app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+// app.use(methodOverride('_methos'))
+
+
+// CONTROLLERS
+app.use('/books', require('./controllers/books_controller.js'))
+
+app.get('/', (req, res) => {
+    res.render('<h1>Hello from Books API</h1>')
+})
+
+// app.get('*', (req, res) => {
+//     res.render('error404')
+// })
+
+// Listen for Connections
+app.listen(PORT, () => console.log("App is running!"))
